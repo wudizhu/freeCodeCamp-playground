2bcfafccf7beac4c2c100a72f42c4cdf06723afa
Feng Zhu <FengZ@devriesretail.com>
2016-12-02 16:00:24 +0100

    tested some javascript implementation

---
+++ Portfolio/scripts/portfolio.js
@@ -0,0 +1,12 @@
+function showNavDropdown() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className += " responsive";
+
+    } else {
+        x.className = "nav";
+    }
+
+
+
+}
